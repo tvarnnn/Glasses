313b0b7f54ba0a04a5433da9e53daacbfa39d5bd
TristanVarner <tvarner3@charlotte.edu>
2026-08-18 17:35:36 -0400

    Initial Commit

---
+++ Glasses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glasses
+//
+//  Created by Tristan Varner on 8/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glasses/GlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesApp.swift
+//  Glasses
+//
+//  Created by Tristan Varner on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
